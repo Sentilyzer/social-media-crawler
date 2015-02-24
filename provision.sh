b445c9dacfe36b63e6f9b63d846159a9d9dcb237
Alexander Ruhland <alexander.b.ruhland@gmail.com>
2015-02-24 01:01:39 +0100

    added vagrant setup

---
+++ provision.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# set system timezone
+echo "Europe/Berlin" | sudo tee /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+
+# update package list and install essential packages
+echo "Updating packages...\n"
+apt-get update -y
+echo "Installing packages...\n"
+apt-get install -y build-essential curl git vim libssl-dev man python python-pip
+
+# add github to known SSH hosts
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
