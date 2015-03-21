390fbd8bb5ae5d95f36e6e385d1b761d6fd52fd4
Alexander Ruhland <alexander.b.ruhland@gmail.com>
2015-03-21 16:57:25 +0100

    added rabbitmq install to vagrant provisioning

--- provision.sh
+++ provision.sh
@@ -12,14 +12,24 @@ dpkg-reconfigure -f noninteractive tzdata
 # add github to known SSH Hosts
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 
+# add APT-Keys and Repositories
+
+sudo add-apt-repository "deb http://www.rabbitmq.com/debian/ stable main"
+wget https://www.rabbitmq.com/rabbitmq-signing-key-public.asc
+sudo apt-key add rabbitmq-signing-key-public.asc
+
 # update package list and install essential packages
 apt-get update -y
+apt-get upgrade -y
 
 # install basic packages
 apt-get install -y build-essential curl git vim libssl-dev man
 
 # install python stuff
 apt-get install -y python python-pip python-software-properties python-dev python-setuptools
+
+# install rabbitmq
+apt-get install -y rabbitmq-server
 
 # mysql configuration variables needed for mysql setup
 sudo debconf-set-selections <<< "mysql-server \
@@ -33,5 +43,5 @@ apt-get install -y mysql-server libmysqlclient-dev
 mysql -uroot -p$DBPASSWD -e "CREATE DATABASE IF NOT EXISTS $DBNAME"
 mysql -uroot -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'localhost' identified by '$DBPASSWD'"
 
-# install social media crawler specific packages
+# install social media crawler specific python packages
 pip install -r /vagrant/requirements.txt
